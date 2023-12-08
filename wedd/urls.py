7998a64e1575a64fb8e25c45ebbaa303045d38c2
Vaibhavdesh20 <111379850+Vaibhavdesh20@users.noreply.github.com>
2023-12-08 12:36:11 +0000

    chan

--- wedd/urls.py
+++ wedd/urls.py
@@ -3,6 +3,7 @@ from .import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('register',views.register,name='register'),
     # path("profile/", profile, name="profile"),
 ]
 
--- wedd/views.py
+++ wedd/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 def home(request):
     return render(request,'home.html')
 
+def register(request):
+    return render(request,'register.html')
+
 # def profile(request):
 #     user = request.user
 
