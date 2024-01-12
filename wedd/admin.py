3b8c5d73e3d82e960491cb901a5d6391451e4846
Vaibhavdesh20 <111379850+Vaibhavdesh20@users.noreply.github.com>
2024-01-12 12:40:42 +0000

    data

--- wedd/admin.py
+++ wedd/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import wandekar
 
 # Register your models here.
+
+class WandekarAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "email","password","confirmPassword","dob","gender",)
+
+admin.site.register(wandekar,WandekarAdmin )
